1823ea023af10c24e6f8f1a5186b0246928abe77
Subha Das <rohan6056@hotmail.com>
2022-07-11 01:34:09 -0700

    Got rid of extra log statement

--- backend/api/auth.ts
+++ backend/api/auth.ts
@@ -1,6 +1,5 @@
 import {Router} from 'express';
 import passport from 'passport';
-import logger from '../logger';
 import {
   passportAuthenticateLocal} from './middleware/login';
 import {extractRegisterInput, registerUser} from './middleware/register';
@@ -9,7 +8,7 @@ import {extractRegisterInput, registerUser} from './middleware/register';
 // eslint-disable-next-line new-cap
 export const authRouter = Router();
 
-authRouter.get('/login/google', () => logger.info('here'),
+authRouter.get('/login/google',
     passport.authenticate('google', {scope: ['profile']}));
 
 authRouter.get('/login/google/callback',
