cb9eeacfbea0c28c8bf3626c4aa0ab7f7f7d3963
Subha Das <rohan6056@hotmail.com>
2023-01-03 20:38:21 -0800

    Removed marketing site from this repo

--- marketing/src/colors.css.ts
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  background: "linear-gradient(116.82deg, #5F00FA 0%, #C808D9 100%)",
-  text: "white",
-  primary: "#F8ACFF",
-  muted: "#f5cc00",
-  active: "#004287",
-  black: "#000",
-}
