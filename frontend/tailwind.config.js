a5ddb256a97ce68a652c3008135d02060a10892f
Subha Das <rohan6056@hotmail.com>
2023-01-06 19:19:35 -0800

    Added tailwind to react project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  corePlugins: {
+    preflight: false,
+  },
+  prefix: 'tw-',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
